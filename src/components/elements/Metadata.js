8d7083fc8431b299401a4b959c830dcb83645bf9
Dhiraj Kumar Sharma <dhirajksharma.social@gmail.com>
2022-11-26 10:16:15 +0530

    Update meta files

--- src/components/elements/Metadata.js
+++ src/components/elements/Metadata.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import Helmet from "react-helmet";
 import NavContext from "./NavContext";
 
-const Metadata =({title, nav}) =>{
+const Metadata =({title, nav, ogimage="https://github.com/dhirajksharma/thelorestore-frontend/blob/main/src/res/thumb.jpg?raw=true"}, ogtitle="The Lore Store", ogdes="An online Book Store created by one Dhiraj K. Sharma") =>{
 
     const { navOption, setNavOption } = useContext(NavContext);
     const setNav=()=>{
@@ -17,6 +17,9 @@ const Metadata =({title, nav}) =>{
         <Helmet>
             <title>{title}</title>
             {setNav()};
+            <meta name="og:image" content={ogimage} />
+            <meta property="og:title" content={ogtitle} />
+            <meta property="og:description" content={ogdes}/>
         </Helmet>
     )
 }
